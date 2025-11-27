99bab4548e138c7d99e2e7660996c190a306617f
James Howat <james@thehowats.net>
2025-11-27 14:12:28 +0000

    Add Year2021, starting with Day 8.

---
+++ Year2021/Day8.cs
@@ -0,0 +1,75 @@
+﻿using Utilities;
+using Xunit;
+
+namespace Year2021;
+
+public class Day8
+{
+    public static int Part1(List<string> lines)
+    {
+        var answer = lines
+            .Select(ParseNote)
+            .Select(FindUniqueDigits)
+            .Sum();
+
+        return answer;
+    }
+
+    private static Note ParseNote(string noteString)
+    {
+        var parts = noteString.Split('|');
+        var patterns = parts[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+        var outputs = parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+        return new Note(patterns, outputs);
+
+    }
+
+    private static int FindUniqueDigits(Note note)
+    {
+        var count = note.Outputs.Where(o =>
+            o.Length == 2 // 1
+            || o.Length == 3 // 7
+            || o.Length == 4 // 4
+            || o.Length == 7 // 8
+            ).Count();
+        return count;
+    }
+
+    public static int Part2(List<string> lines)
+    {
+
+        return 0;
+    }
+
+    [Theory]
+    [InlineData("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")]
+    public void Day8_Part1_Example1(string note)
+    {
+        Assert.Equal(0, Part1([note]));
+    }
+
+    [Fact]
+    public void Day8_Part1_Example2()
+    {
+        Assert.Equal(26, Part1(Input.Strings(@"day8example2.txt")));
+    }
+
+    [Fact]
+    public void Day8_Part2_Example()
+    {
+        Assert.Equal(0, Part2(Input.Strings(@"day8example3.txt")));
+    }
+
+}
+
+internal class Note
+{
+    public List<string> Patterns { get; private set; }
+    public List<string> Outputs { get; private set; }
+
+    public Note(List<string> patterns, List<string> outputs)
+    {
+        Patterns = patterns;
+        Outputs = outputs;
+    }
+}
---
+++ Year2021/Program.cs
@@ -0,0 +1,5 @@
+﻿using Utilities;
+using Year2021;
+
+Runner.Run("Day 8, Part 1", () => Day8.Part1(Input.Strings(@"day8input.txt")));
+//Runner.Run("Day 8, Part 2", () => Day8.Part2(Input.Strings(@"day8input.txt")));
