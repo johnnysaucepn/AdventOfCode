a34ffaa2b2267b7c7510b6b1b6a4504e27779193
James Howat <james@thehowats.net>
2025-12-03 08:04:21 +0000

    Year 2025, Day 2, Part 1 and start on Part 2.

---
+++ Year2025/Day2.cs
@@ -0,0 +1,84 @@
+﻿using Utilities;
+using Xunit;
+
+namespace Year2025
+{
+    public class Day2
+    {
+        public long Part1(List<string> lines)
+        {
+            var ranges = lines
+                .SelectMany(l => l.Split(','))
+                .Select(r =>
+                {
+                    var pair = r.Split('-');
+                    return (start: long.Parse(pair[0]), end: long.Parse(pair[1]));
+                });
+            var invalids = ranges
+                .SelectMany(p => FindInvalid(p.start, p.end, IsInvalidDouble));
+
+            return invalids.Sum();
+        }
+
+        public int Part2(List<string> lines)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<long>? FindInvalid(long start, long end, Func<long, bool> checkFunction)
+        {
+            for (var i = start; i <= end; i++)
+            {
+                if (checkFunction(i)) yield return i;
+            }
+        }
+
+        private bool IsInvalidDouble(long candidate)
+        {
+            var candidateString = candidate.ToString();
+            if (candidateString.Length % 2 == 1) return false; // odd length cannot have two pairs;
+            var half = candidateString.Length / 2;
+            if (candidateString[..half] == candidateString[half..]) return true;
+            return false;
+        }
+
+        private bool IsInvalidTuple(long candidate)
+        {
+            var candidateString = candidate.ToString();
+
+            return false;
+        }
+
+        [Theory]
+        [InlineData(11, true)]
+        [InlineData(22, true)]
+        [InlineData(1010, true)]
+        [InlineData(1188511885, true)]
+        [InlineData(1, false)]
+        [InlineData(101, false)]
+        [InlineData(446447, false)]
+        public void Day2_IsInvalid(long candidate, bool expected)
+        {
+            Assert.Equal(expected, IsInvalid(candidate));
+        }
+
+        [Theory]
+        [InlineData(11, 22, new long[] { 11, 22 })]
+        public void Day2_FindInvalid(long start, long end, long[] expected)
+        {
+            Assert.Equal(expected, FindInvalid(start, end));
+        }
+
+        [Fact]
+        public void Day2_Part1_Example()
+        {
+            Assert.Equal(1227775554, Part1(Input.Strings(@"day2example.txt")));
+        }
+
+        [Fact]
+        public void Day2_Part2_Example()
+        {
+            Assert.Equal(0, Part2(Input.Strings(@"dayXexample2.txt")));
+        }
+    }
+}
--- Year2025/Program.cs
+++ Year2025/Program.cs
@@ -3,3 +3,5 @@ using Year2025;
 
 Runner.Run("Day 1, Part 1", () => new Day1().Part1(Input.Strings(@"day1input.txt")));
 Runner.Run("Day 1, Part 2", () => new Day1().Part2(Input.Strings(@"day1input.txt")));
+
+Runner.Run("Day 2, Part 1", () => new Day2().Part1(Input.Strings(@"day2input.txt")));
