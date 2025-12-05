097305f40c65dd333d0dd56d501aa1376ab1003a
James Howat <james@thehowats.net>
2025-12-05 00:52:20 +0000

    Tweak template name, and usually only one example.

--- Utilities/DayTemplate.cs
+++ Utilities/DayTemplate.cs
@@ -14,15 +14,15 @@ public class Template
     }
 
     [Fact]
-    public void DayX_Part1_Example1()
+    public void DayX_Part1_Example()
     {
-        Assert.Equal(0, Part1(Input.Strings(@"dayXexample1.txt")));
+        Assert.Equal(0, Part1(Input.Strings(@"dayXexample.txt")));
     }
 
     [Fact]
-    public void DayX_Part2_Example2()
+    public void DayX_Part2_Example()
     {
-        Assert.Equal(0, Part2(Input.Strings(@"dayXexample2.txt")));
+        Assert.Equal(0, Part2(Input.Strings(@"dayXexample.txt")));
     }
 
 }
